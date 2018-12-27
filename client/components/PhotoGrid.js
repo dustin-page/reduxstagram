9e2e10c356bce9f541bd223f1d692a5dd0fc4c1f
Dustin Page <dustinpage@live.com>
2018-12-27 17:16:03 -0600

    Code the Photo Component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Photo from './Photo';
+
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                I'm the photo grid
+
+                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+
+                {/* <pre>
+                    {JSON.stringify(this.props.posts, null, ' ')}
+                </pre> */}
             </div>
         )
     }
