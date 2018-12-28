622d8f709d87f67e8153ae84aaef6f8b175d1e1e
Dustin Page <dustinpage@live.com>
2018-12-28 08:50:25 -0600

    Clean up Comment component code

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -3,14 +3,11 @@ import React from 'react';
 const Comments = React.createClass({
 
     //create a render function when it doesn't make sense to create a separate component
-    renderComment (comment, i) {
-        console.log(comment);
+    renderComment(comment, i) {
         return (
             <div className="comment">
                 <p>
-                    <strong>
-                        {comment.user}
-                    </strong>
+                    <strong>{comment.user}</strong>
                     {comment.text}
                     <button className="remove-comment">&times;</button>
                 </p>
@@ -21,10 +18,12 @@ const Comments = React.createClass({
     render() {
         return (
             <div className="comments">
+
                 {this.props.postComments.map(this.renderComment)}
+
                 <form ref="commentForm" className="comment-form">
-                    <input type="text" ref="author" placeholder="author"/>
-                    <input type="text" ref="comment" placeholder="comment"/>
+                    <input type="text" ref="author" placeholder="author" />
+                    <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
                 </form>
             </div>
