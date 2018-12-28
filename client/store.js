b8c3f51e2d66458a4ef18711be4c697410508717
Dustin Page <dustinpage@live.com>
2018-12-28 15:18:16 -0600

    Add support for Redux devtools

--- client/store.js
+++ client/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import {browserHistory} from 'react-router';
+import { browserHistory } from 'react-router';
 
 //import the root reducer
 import rootReducer from './reducers/index';
@@ -15,7 +15,13 @@ const defaultState = {
     comments
 }
 
-const store = createStore(rootReducer, defaultState);
+//Enable support for Redux dev tools
+//If devtools is not open just return the store "f"
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
